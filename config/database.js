dd7a923a32eb48e06f47c12bc9a33d545e6c6547
曹胜楠 <caoshengnan@caoshengnandeMacBook-Air.local>
2018-11-08 23:46:30 -0500

    initial commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // configure the code below with your username, password and mlab database information
+  database: 'mongodb://side:ss2154@ds151383.mlab.com:51383/pprpapp',   //prod
+  //database: 'mongodb://localhost:27017/meanauth',    //dev
+  secret: 'yoursecret'
+}
